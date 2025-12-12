1ba2a93d47c646ed34677bc86bc4f7af8b7b342a
jricardost <j-ricardost@hotmail.com>
2025-12-12 07:48:28 -0300

    fix: database creation file

--- backend/sql/DatabaseSetup.js
+++ backend/sql/DatabaseSetup.js
@@ -15,17 +15,6 @@ async function main() {
 
         /* Create Database */
         console.log("creating database...")
-        await connection.query(fs.readFileSync(path.join(__dirname, 'create_database.sql'), 'utf8'))
-        await connection.query('USE 21daysapp')
-
-        // console.log("creating challenges table (CHALLENGES)...")
-        // await connection.query(fs.readFileSync(path.join(__dirname, 'create_table_challenges.sql'), 'utf8'))
-        // console.log("creating password reset table (PASSWORD_RESET)...")
-        // await connection.query(fs.readFileSync(path.join(__dirname, 'create_table_password_reset.sql'), 'utf8'))
-        // console.log("creating users table (USERS)...")
-        // await connection.query(fs.readFileSync(path.join(__dirname, 'create_table_users.sql'), 'utf8'))
-
-        console.log("creating tables...")
         await connection.query(fs.readFileSync(path.join(__dirname, 'tables.sql'), 'utf8'))
 
         await connection.end()
